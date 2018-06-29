38a2d5a1ffd4fda5627445a0e8fb8aab84277f55
matte <matte@157.27.139.223>
2018-06-29 09:09:14 +0200

    settimo caricamento

--- src/progettoingegneriasw/NegozioController.java
+++ src/progettoingegneriasw/NegozioController.java
@@ -7,6 +7,7 @@ package progettoingegneriasw;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -16,6 +17,7 @@ public class NegozioController implements ActionListener {
     
     private Negozio negozio;
     private NegozioView negozioView;
+    private OrdineView new_ordine = new OrdineView();
     
     private String[] utenti={"segreteria","magazzino","gestioneNegozio"};
     private String[] password={"ciao1234","salut1234","privet1234"};
@@ -25,10 +27,13 @@ public class NegozioController implements ActionListener {
         
         this.negozio = negozio;
         this.negozioView = negView;
-        negView.setBounds(310, 310, 360, 290);
-        negView.addActionListener(this);
-        negView.setVisible(true);
-        negView.setResizable(false);
+        negozioView.setBounds(310, 310, 360, 290);
+        negozioView.addActionListener(this);
+        negozioView.setVisible(true);
+        negozioView.setResizable(false);
+        new_ordine.addActionListeners(this);
+        new_ordine.setResizable(false);
+        new_ordine.setBounds(310, 310, 400, 400);
         
         
     }
@@ -43,14 +48,20 @@ public class NegozioController implements ActionListener {
             LoginController login = new LoginController(new LoginView(), new Login(utenti, password));
         }
         
-        if( e.getSource() == negozioView.visualizza_ordini){
-            if( negozioView.shop_list.getSelectedIndex() == -1 ){
-                
-            }
-            
+        if( e.getSource() == negozioView.inserisci_ordine){
+            negozioView.setVisible(false);
+            new_ordine.setVisible(true);
         }
         
         if( e.getSource() == negozioView.shop_list ){
+            String codFiscale = "";
+            codFiscale = JOptionPane.showInputDialog(null, "Inserire il codice fiscale per confermare la propria identità");
+            //controllo codice Fiscale
+            
+            if( negozioView.shop_list.getSelectedIndex() != -1 ){
+                negozioView.inserisci_ordine.setEnabled(true);
+                negozioView.visualizza_ordini.setEnabled(true);
+            }
             
         }
         
